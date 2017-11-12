d0a1ecd0b04873edb8e00b272f9e6ebf09ac1b11
j-christoffersen <jacksontchristo@gmail.com>
2017-11-11 16:07:27 -0800

    dummy data

--- client/src/app/components/app.js
+++ client/src/app/components/app.js
@@ -1,8 +1,23 @@
 angular.module('lyft-vs-uber')
 
 .component('app', {
-  controller: function() {
+  controller: function(/* rideInfo */) {
+    // rideInfo.get({start:{lat: 37.7836924, lng:-122.4089666}, end:{lat: 37.7838038, lng:-122.3989875}})
+    // .then(data => {
+    //   this.rideInfo = data;
+    // });
     
+    // //dummy data
+    this.rideInfo = {
+      uber: {
+        price: 26,
+        time: 20
+      },
+      lyft: {
+        price: 31,
+        time: 15
+      }
+    }
   },
   
   templateUrl: 'app/templates/app.html'
