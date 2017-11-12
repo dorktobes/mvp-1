2c844492f0a7bac851084edd06f3f01c1579e3dd
j-christoffersen <jacksontchristo@gmail.com>
2017-11-12 11:39:04 -0800

    send request to server for ride data

--- client/src/app/services/rideInfo.js
+++ client/src/app/services/rideInfo.js
@@ -1,5 +1,14 @@
 angular.module('lyft-vs-uber')
 
-.service('rideInfo', function() {
-  
+.service('rideInfo', function($http) {
+  this.getInfo = params => {
+    return $http({
+      method: 'GET',
+      url: '/rideInfo',
+      headers: {
+        'Data-Type': 'application/JSON'
+      },
+      params: params
+    }).then(res => res.data); 
+  };
 });
