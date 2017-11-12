9ad41dc2c4d39a3b3b3efc925a3d578784c405d4
j-christoffersen <jacksontchristo@gmail.com>
2017-11-11 17:07:10 -0800

    implement front end ajax call to rideInfo

--- client/src/app/components/app.js
+++ client/src/app/components/app.js
@@ -1,23 +1,24 @@
 angular.module('lyft-vs-uber')
 
 .component('app', {
-  controller: function(/* rideInfo */) {
-    // rideInfo.get({start:{lat: 37.7836924, lng:-122.4089666}, end:{lat: 37.7838038, lng:-122.3989875}})
-    // .then(data => {
-    //   this.rideInfo = data;
-    // });
+  controller: function(rideInfo) {
+    rideInfo.getInfo({start:{lat: 37.7836924, lng:-122.4089666}, end:{lat: 37.7838038, lng:-122.3989875}})
+    .then(data => {
+      console.log(data);
+      this.rideInfo = data;
+    });
     
     // //dummy data
-    this.rideInfo = {
-      uber: {
-        price: 26,
-        time: 20
-      },
-      lyft: {
-        price: 31,
-        time: 15
-      }
-    }
+    // this.rideInfo = {
+    //   uber: {
+    //     price: 26,
+    //     time: 20
+    //   },
+    //   lyft: {
+    //     price: 31,
+    //     time: 15
+    //   }
+    // }
   },
   
   templateUrl: 'app/templates/app.html'
